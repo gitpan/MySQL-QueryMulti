d76163215f3e1bdeb7ab9f4155cb1c90f74bd7f3
John Gravatt <gravattj@cpan.org>
2012-12-11 14:59:13 +0000

    Import of GRAVATTJ/MySQL-QueryMulti-0.03 from CPAN.
    gitpan-cpan-distribution: MySQL-QueryMulti
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         GRAVATTJ/MySQL-QueryMulti-0.03.tar.gz
    gitpan-cpan-author:       GRAVATTJ
    gitpan-cpan-maturity:     released

--- lib/MySQL/QueryMulti.pm
+++ lib/MySQL/QueryMulti.pm
@@ -13,11 +13,11 @@ MySQL::QueryMulti - module for querying multiple MySQL databases in parallel
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
